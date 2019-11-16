787a0451d99f60c68ee0e17de1884c71399be4da
Red <e.sternvik@gmail.com>
2019-11-16 15:02:26 +0100

    Add camera movement and rotation support

--- src/Components/Camera.cpp
+++ src/Components/Camera.cpp
@@ -1,5 +1,7 @@
 #include "Camera.h"
 #include <glm/gtc/matrix_transform.hpp>
+#include "../GameObject.h"
+#include "Transform.h"
 
 namespace glGame {
 
@@ -9,7 +11,12 @@ namespace glGame {
 	}
 
 	void Camera::update() {
-		
+		Vector3 position = gameObject->transform->position;
+		Vector3 rotation = gameObject->transform->rotation;
+		m_viewMatrix = glm::rotate(glm::mat4(1.0), glm::radians(rotation.x), glm::vec3(1, 0, 0));
+		m_viewMatrix *= glm::rotate(glm::mat4(1.0), glm::radians(rotation.y), glm::vec3(0, 1, 0));
+		m_viewMatrix *= glm::rotate(glm::mat4(1.0), glm::radians(rotation.z), glm::vec3(0, 0, 1));
+		m_viewMatrix *= glm::translate(glm::mat4(1.0), glm::vec3(position.x, position.y, position.z));
 	}
 
 }
