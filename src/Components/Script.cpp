66fe3c6cd552b3be04ca8c4e9f85d577073150fe
Red <e.sternvik@gmail.com>
2020-04-08 18:08:23 +0200

    Fix last commit

--- src/Components/Script.cpp
+++ src/Components/Script.cpp
@@ -87,8 +87,8 @@ namespace glGame {
 
     void Script::update(float deltatime) {
         if(m_asScriptUpdateFunction != nullptr) {
-            m_asScriptContext->SetArgFloat(0, deltatime);
             m_asScriptContext->Prepare(m_asScriptUpdateFunction);
+            m_asScriptContext->SetArgFloat(0, deltatime);
             int r = m_asScriptContext->Execute();
             if(r != asEXECUTION_FINISHED) std::cout << "Could not execute" << std::endl;
         }
