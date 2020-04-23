f9f8024a489c0242bdc8bc40ebf20e83f95b4fe2
Red <e.sternvik@gmail.com>
2020-04-23 21:11:59 +0200

    Show assets in asset window

--- src/Editor/AssetWindow.cpp
+++ src/Editor/AssetWindow.cpp
@@ -20,8 +20,28 @@ namespace glGame {
     void AssetWindow::renderWindow() {
         if(m_assetManager == nullptr) return;
 
-        for(auto it = m_assetManager->getAssetsBegin(); it != m_assetManager->getAssetsEnd(); ++it) {
-            ImGui::Text(it->second->name.c_str());
+        ImGuiStyle& style = ImGui::GetStyle();
+        float window_visible = ImGui::GetWindowPos().x + ImGui::GetWindowContentRegionMax().x;
+        int number = 0, totalAssetCount = m_assetManager->assetCount();
+        float currentLineX = 0.0;
+        for(auto it = m_assetManager->getAssetsBegin(); it != m_assetManager->getAssetsEnd(); ++it, ++number) {
+            ImVec2 buttonSize = ImGui::CalcTextSize(it->second->name.c_str());
+            buttonSize = ImVec2(buttonSize.x + 25, 50);
+
+            currentLineX += buttonSize.x + style.ItemSpacing.x;
+            if(currentLineX < window_visible) ImGui::SameLine();
+            else currentLineX = 0.0;
+
+            ImGui::PushID(number);
+            ImGui::Button(it->second->name.c_str(), buttonSize);
+            ImGui::PopID();
+
+            unsigned int data = it->first;
+            if(ImGui::BeginDragDropSource(ImGuiDragDropFlags_None)) {
+                ImGui::SetDragDropPayload(it->second->getAssetTypeString(), &data, sizeof(unsigned int));
+                ImGui::Text(it->second->getAssetTypeString());
+                ImGui::EndDragDropSource();
+            }
         }
     }
 
