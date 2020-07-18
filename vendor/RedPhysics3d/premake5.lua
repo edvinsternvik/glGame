242f23ce6276ab0fa2235279cda582459d67c409
RedE01 <e.sternvik@gmail.com>
2020-07-18 13:41:30 +0200

    Upgrade RedPhysics3d to dbf57b8

--- vendor/RedPhysics3d/premake5.lua
+++ vendor/RedPhysics3d/premake5.lua
@@ -1,6 +1,7 @@
 project "RedPhysics3d"
 	kind "StaticLib"
 	language "C++"
+	systemversion "latest"
 
 	targetdir "bin/%{cfg.buildcfg}"
 	objdir "bin-int/%{cfg.buildcfg}"
@@ -9,3 +10,11 @@ project "RedPhysics3d"
 		"RedPhysics3d/**.cpp",
 		"RedPhysics3d/**.h"
 	}
+
+	filter "configurations:Debug"
+		symbols "On"
+		runtime "Debug"
+
+	filter "configurations:Release"
+		optimize "On"
+		runtime "Release"
