67174ccd44774d36af7773302fa2e36800c8bac6
RedE01 <e.sternvik@gmail.com>
2020-08-11 19:27:18 +0200

    Fix screen flashing when picking objects

--- src/Editor/ObjectPicker.cpp
+++ src/Editor/ObjectPicker.cpp
@@ -72,6 +72,7 @@ namespace glGame {
 
         m_colorPickerFramebuffer->unbind();
         glClearColor(clearColor[0], clearColor[1], clearColor[2], clearColor[3]);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     }
 
     Vector3i calculateColorFromId(unsigned int id) {
